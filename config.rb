2de763eacf584d6d39a9bda6ae5653e62a9f6500
Brent Hargrave <brent@brent.is>
2017-01-31 15:40:43 -0500

    setup '/posts' namespace for blog entries

--- config.rb
+++ config.rb
@@ -22,8 +22,7 @@ page '/*.txt', layout: false
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
+  blog.prefix = "posts"
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
@@ -35,10 +34,8 @@ activate :blog do |blog|
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
-
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
-
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
